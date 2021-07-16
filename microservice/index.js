6f42edd4ad8b485388e82c2e3925720e275d0b25
ruben4181 <ruben4181@gmail.com>
2021-07-16 17:55:28 -0500

    With rice

--- microservice/index.js
+++ microservice/index.js
@@ -2,12 +2,85 @@ const express = require("express");
 const app = express();
 var cors = require('cors');
 const port = 1217;
+const axios = require("axios");
+const API_KEY = "$2b$10$X0kbxQICfdAyqkQCj6Lv3ugD2uvhllWkbr8mywZyx5s5llC9nbxCy";
 
 app.use(cors());
 
+app.get("/update-news-beans", (req, res)=>{
+  let config = {
+    url : "https://api.jsonbin.io/v3/b",
+    method : "post",
+    data : require("./scrapping_data/bean_news.json"),
+    headers : {
+      "Content-Type" : "application/json",
+      "X-Master-Key" : API_KEY,
+      "X-Bin-Private" : false,
+      "X-Bin-Name" : "beans.json"
+    }
+  }
+  axios(config).then((resp)=>{
+    res.json(resp.data);
+  }).catch((err)=>{
+    res.status(500);
+    res.json(err);
+  });
+});
+
+app.get("/update-news-musa", (req, res)=>{
+  let config = {
+    url : "https://api.jsonbin.io/v3/b",
+    method : "post",
+    data : require("./scrapping_data/musa_news.json"),
+    headers : {
+      "Content-Type" : "application/json",
+      "X-Master-Key" : API_KEY,
+      "X-Bin-Private" : false,
+      "X-Bin-Name" : "musa.json"
+    }
+  }
+  axios(config).then((resp)=>{
+    res.json(resp.data);
+  }).catch((err)=>{
+    res.status(500);
+    res.json(err);
+  });
+});
+
+app.get("/update-news-cassava", (req, res)=>{
+  let config = {
+    url : "https://api.jsonbin.io/v3/b",
+    method : "post",
+    data : require("./scrapping_data/cassava_news.json"),
+    headers : {
+      "Content-Type" : "application/json",
+      "X-Master-Key" : API_KEY,
+      "X-Bin-Private" : false,
+      "X-Bin-Name" : "cassava.json"
+    }
+  }
+  axios(config).then((resp)=>{
+    res.json(resp.data);
+  }).catch((err)=>{
+    res.status(500);
+    res.json(err);
+  });
+});
+
 app.get("/news-beans", (req, res)=>{
-  let response = require("./scrapping_data/bean_news.json");
-  res.json(response);
+  let config = {
+    url : "https://api.jsonbin.io/v3/b/60e0b73755b7245a20d4ba71/latest",
+    method : "get",
+    headers : {
+      "X-Master-key" : "$2b$10$X0kbxQICfdAyqkQCj6Lv3ugD2uvhllWkbr8mywZyx5s5llC9nbxCy"
+    }
+  }
+  axios(config).then((resp)=>{
+    res.json(resp.data);
+  }).catch((err)=>{
+    res.status(200);
+    res.json(err);
+  })
 });
 
 app.get("/news-musa", (req, res)=>{
--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+import requests
+
 import json
 
 def loadDriver():
@@ -13,7 +15,7 @@ def loadDriver():
         #chrome_options.add_argument("disable-gpu")
         chrome_options.add_argument("window-size=1500, 700")
         #chrome_options.add_argument("--user-data-dir=./infografia_profile")
-        driver = webdriver.Chrome(executable_path='./drivers/chromedriver', options=chrome_options)       
+        driver = webdriver.Chrome(executable_path='./drivers/chromedriver', options=chrome_options)
         #driver = webdriver.Firefox()
         driver.get("https://www.google.com")
         print("Driver cargado")
@@ -27,25 +29,26 @@ def loadDriver():
         print(e)
         x=1/0
         return []
-        
+
 def loadNews(driver, query):
 	driver.get(query)
 	news = []
 	solid_news = driver.find_elements_by_tag_name("g-card")
 	for sn in solid_news:
 		title = sn.find_elements_by_xpath('.//*[@class="JheGif nDgy9d"]')
-		body = sn.find_elements_by_xpath('.//*[@class="Y3v8qd"]')
+		description = sn.find_elements_by_xpath('.//*[@class="Y3v8qd"]')
 		time = sn.find_elements_by_xpath('.//*[@class="WG9SHc"]')
 		img = sn.find_elements_by_xpath('.//img[@height="112"]')
 		src = sn.find_elements_by_xpath('.//*[@class="XTjFC WF4CUc"]')
+		url = sn.find_elements_by_xpath('.//a')[0].get_attribute('href')
 		if(len(title)==0):
 			print("Excluded beacuse of title")
 			continue
 			#print(title[0].text)
-		if(len(body)==0):
-			print("Excluded beacuse of body")
+		if(len(description)==0):
+			print("Excluded beacuse of description")
 			continue
-			#print(body[0].text)
+			#print(description[0].text)
 		if(len(time)==0):
 			print("Excluded beacuse of time")
 			continue
@@ -59,10 +62,11 @@ def loadNews(driver, query):
 			src = src[0].text
 		new = {
 			"title" : title[0].text,
-			"body" : body[0].text,
+			"description" : description[0].text,
 			"time" : time[0].text,
 			"img" : img[0].get_attribute("src"),
-			"src" : src
+			"src" : src,
+			"url" : url
 		}
 		news.append(new)
 	return news
@@ -70,9 +74,59 @@ def main():
 	beans_query = 'https://www.google.com/search?igu=1&ei=&lr=lang_en&q=(CGIAR+OR+"Africa+Rice+Center"+OR+CIFOR+OR+ICARDA+OR+ICRISAT+OR+IFPRI+OR+IITA+OR+ILRI+OR+CIMMYT+OR+CIP+OR+IRRI+OR+IWMI+OR+Bioversity+OR+CIAT+OR+"World+Agroforestry"+OR+ICRAF+OR+WorldFish)+AND+(frejol+OR+frijol+OR+phaseolus+OR+frejol)&rlz=1C1CHBF_enCO920CO920&tbm=nws&sxsrf=ALeKk02RKFVlYZDHreEzANSw2d6ib5cf_w:1620406855719&source=lnt&sa=X&ved=0ahUKEwjQ9pr4hbjwAhUPTjABHQBWBKIQpwUIKg&biw=1920&bih=880&dpr=1&tbas=0'
 	musa_query = 'https://www.google.com/search?igu=1&ei=&lr=lang_en&q=(CGIAR+OR+"Africa+Rice+Center"+OR+CIFOR+OR+ICARDA+OR+ICRISAT+OR+IFPRI+OR+IITA+OR+ILRI+OR+CIMMYT+OR+CIP+OR+IRRI+OR+IWMI+OR+Bioversity+OR+CIAT+OR+"World+Agroforestry"+OR+ICRAF+OR+WorldFish)+AND+(bananas+OR+bananos+OR+banana+OR+musa)&rlz=1C1CHBF_enCO920CO920&tbm=nws&sxsrf=ALeKk02RKFVlYZDHreEzANSw2d6ib5cf_w:1620406855719&source=lnt&sa=X&ved=0ahUKEwjQ9pr4hbjwAhUPTjABHQBWBKIQpwUIKg&biw=1920&bih=880&dpr=1&tbas=0'
 	cassava_query = 'https://www.google.com/search?igu=1&ei=&lr=lang_en&q=(CGIAR+OR+"Africa+Rice+Center"+OR+CIFOR+OR+ICARDA+OR+ICRISAT+OR+IFPRI+OR+IITA+OR+ILRI+OR+CIMMYT+OR+CIP+OR+IRRI+OR+IWMI+OR+Bioversity+OR+CIAT+OR+"World+Agroforestry"+OR+ICRAF+OR+WorldFish)+AND+(cassava+OR+yuca+OR+manihot+OR+mandioca)&rlz=1C1CHBF_enCO920CO920&tbm=nws&sxsrf=ALeKk02RKFVlYZDHreEzANSw2d6ib5cf_w:1620406855719&source=lnt&sa=X&ved=0ahUKEwjQ9pr4hbjwAhUPTjABHQBWBKIQpwUIKg&biw=1920&bih=880&dpr=1&tbas=0'
+	rice_query = 'https://www.google.com/search?igu=1&ei=&lr=lang_en&q=(CGIAR+OR+"Africa+Rice+Center"+OR+CIFOR+OR+ICARDA+OR+ICRISAT+OR+IFPRI+OR+IITA+OR+ILRI+OR+CIMMYT+OR+CIP+OR+IRRI+OR+IWMI+OR+Bioversity+OR+CIAT+OR+"World+Agroforestry"+OR+ICRAF+OR+WorldFish)+AND+(arroz+OR+rice)&rlz=1C1CHBF_enCO920CO920&tbm=nws&sxsrf=ALeKk02RKFVlYZDHreEzANSw2d6ib5cf_w:1620406855719&source=lnt&sa=X&ved=0ahUKEwjQ9pr4hbjwAhUPTjABHQBWBKIQpwUIKg&biw=1920&bih=880&dpr=1&tbas=0'
+	
 	driver = loadDriver()
+	API_KEY = "$2b$10$X0kbxQICfdAyqkQCj6Lv3ugD2uvhllWkbr8mywZyx5s5llC9nbxCy"
 	
 	bean_news = loadNews(driver, beans_query)
+	beans_id = "60e0b73755b7245a20d4ba71"
+	url = 'https://api.jsonbin.io/v3/b/'+beans_id
+	headers = {
+		'Content-Type': 'application/json',
+		'X-Master-Key': API_KEY
+	}
+	data = bean_news
+	req = requests.put(url, json=data, headers=headers)
+	print(req.text)
+	
+	musa_news = loadNews(driver, musa_query)
+	
+	musa_id="60e0b9af55b7245a20d4bba9"
+	
+	url = 'https://api.jsonbin.io/v3/b/'+musa_id
+	headers = {
+		'Content-Type': 'application/json',
+		'X-Master-Key': API_KEY
+	}
+	data = musa_news
+	req = requests.put(url, json=data, headers=headers)
+	print(req.text)
+	
+	cassava_news = loadNews(driver, cassava_query)
+	cassava_id="60e0b9c755b7245a20d4bbb7"
+	
+	url = 'https://api.jsonbin.io/v3/b/'+cassava_id
+	headers = {
+		'Content-Type': 'application/json',
+		'X-Master-Key': API_KEY
+	}
+	data = cassava_news
+	req = requests.put(url, json=data, headers=headers)
+	print(req.text)
+	
+	rice_news = loadNews(driver, rice_query)
+	rice_id="60f20973c1256a01cb70e79a"
+	
+	url = 'https://api.jsonbin.io/v3/b/'+rice_id
+	headers = {
+		'Content-Type': 'application/json',
+		'X-Master-Key': API_KEY
+	}
+	data = rice_news
+	req = requests.put(url, json=data, headers=headers)
+	print(req.text)
+	"""
 	with open("./microservice/scrapping_data/bean_news.json", "w") as write_file:
 		json.dump(bean_news, write_file)
 	sleep(5)
@@ -84,5 +138,6 @@ def main():
 	with open("./microservice/scrapping_data/cassava_news.json", "w") as write_file:
 		json.dump(cassava_news, write_file)
 	sleep(5)
-	
+	"""
+
 main()
